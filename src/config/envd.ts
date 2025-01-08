2f853ec8230c8c82e397c14680a3b115c8dfefa0
Joseph Infante <josephinfante.y@gmail.com>
2025-01-07 20:05:51 -0500

    adding new env variable

--- src/config/envd.ts
+++ src/config/envd.ts
@@ -2,6 +2,7 @@
 export const NODE_ENV = process.env.NODE_ENV || 'development'
 export const PORT = Number(process.env.PORT) || 3000
 export const ACCEPTED_ORIGINS = process.env.ACCEPTED_ORIGINS?.split(',') || ['http://localhost:3000']
+export const JWT_SECRET = process.env.JWT_SECRET || 'secret'
 
 // DATABASE VARIABLES
 export const DB_NAME = process.env.DB_NAME || ''
@@ -15,4 +16,4 @@ export const DB_POOL_SIZE_MIN = Number(process.env.DB_POOL_SIZE_MIN) || 0
 export const DB_POOL_SIZE_MAX = Number(process.env.DB_POOL_SIZE_MAX) || 5
 export const DB_POOL_ACQUIRE = Number(process.env.DB_POOL_ACQUIRE) || 30000
 export const DB_POOL_IDLE = Number(process.env.DB_POOL_IDLE) || 10000
-export const DB_POOL_LOGGING = process.env.DB_POOL_LOGGING === 'true'
+export const DB_POOL_LOGGING = process.env.DB_POOL_LOGGING == 'true'
