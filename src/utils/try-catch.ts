17471e1a219bd1cb92fffd0bacf4b5e1822abb82
Joseph Infante <josephinfante.y@gmail.com>
2025-01-08 14:27:20 -0500

    creating a tryCatch util to pass errors to middleware

---
+++ src/utils/try-catch.ts
@@ -0,0 +1,9 @@
+import { NextFunction, Request, Response } from 'express'
+
+export const tryCatch = (controller: Function) => async (req: Request, res: Response, next: NextFunction) => {
+	try {
+		await controller(req, res)
+	} catch (error) {
+		return next(error)
+	}
+}
