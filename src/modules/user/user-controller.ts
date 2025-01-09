4907293d9ad7aebf261291656d4dc5dba194ba98
Joseph Infante <josephinfante.y@gmail.com>
2025-01-09 13:19:13 -0500

    removing create, and extracting the id from the res.locals.user

--- src/modules/user/user-controller.ts
+++ src/modules/user/user-controller.ts
@@ -9,36 +9,27 @@ export class UserController {
 		this.userService = userService
 	}
 
-	create = tryCatch(async (req: Request, res: Response) => {
-		const { status, message } = await this.userService.create({ data: req.body })
-		res.status(status).json({ message })
-	})
-
 	update = tryCatch(async (req: Request, res: Response) => {
-		const { id } = req.params
+		const { id } = res.locals.user
 		const { status, message } = await this.userService.update({ id, data: req.body })
 		res.status(status).json({ message })
 	})
 
-	delete = tryCatch(async (req: Request, res: Response) => {
-		const { id } = req.params
+	delete = tryCatch(async (_req: Request, res: Response) => {
+		const { id } = res.locals.user
 		const { status, message } = await this.userService.delete({ id })
 		res.status(status).json({ message })
 	})
 
-	findById = tryCatch(async (req: Request, res: Response) => {
-		const { id } = req.params
-		const { status, message, data } = await this.userService.findById({ id })
-
-		const response = message ? { message } : data
-		res.status(status).json(response)
+	findById = tryCatch(async (_req: Request, res: Response) => {
+		const { id } = res.locals.user
+		const { status, data } = await this.userService.findById({ id })
+		res.status(status).json(data)
 	})
 
 	findAll = tryCatch(async (req: Request, res: Response) => {
 		const { query } = req
-		const { status, message, data } = await this.userService.findAll({ query })
-
-		const response = message ? { message } : data
-		res.status(status).json(response)
+		const { status, data } = await this.userService.findAll({ query })
+		res.status(status).json(data)
 	})
 }
